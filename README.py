5adef772ea64d5c4944e3afd0556dbe683f50665
Kartik Dhiman <80577980+kartik1600@users.noreply.github.com>
2021-07-04 19:59:25 +0530

    jarvis.py

---
+++ README.py
@@ -0,0 +1,45 @@
+
+
+if N['x']=="hi":
+	print(f"Hi ! {name} , what can i do for you !")
+elif N['x']=="hi jarvis":
+	print(f"HI,{name} !")
+elif N['x']=="who are you?":
+	print("My name is jarvis and i am developed by Mr.Kartik dhiman. ")
+elif N['x']=="tell my age?":
+	print(f"you are ,{age} ! {name}.")
+
+elif N['x']=="how are you jarvis":
+	print(f"I am good , what about you {name}..  ")
+
+elif N['x']=="how are you ":
+	print("i am good")
+
+elif N['x']=="fine":
+	print("thats good !")
+
+elif N['x']=="":
+	print("")
+
+elif N['x']=="":
+	print("")
+
+elif N['x']=='':
+	print()
+
+elif N['x']=='':
+	print()
+
+elif N['x']=='':
+	print()
+
+elif N['x']=='':
+	print()
+
+elif N['x']=='':
+	print()
+
+
+	
+else:
+	print(" i did't get it !")
